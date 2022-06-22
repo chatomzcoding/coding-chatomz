894aada2d752f37e0f5c13b519abe3ebb0b9bacc
chatomzcoding <coding.chatomz@gmail.com>
2022-06-22 10:10:34 +0700

    update cpanel

--- config/app.php
+++ config/app.php
@@ -54,9 +54,9 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost/chatomz/coding'),
 
-    'asset_url' => env('ASSET_URL', null),
+    'asset_url' => env('ASSET_URL', 'http://localhost/chatomz/coding/public'),
 
     /*
     |--------------------------------------------------------------------------
